0435f9e1d18797db7702e71b60acc9a9859458ae
huangyan110110114 <huangyan110110114@jd.com>
2020-08-31 11:50:22 +0800

    监听器：实现SmartApplicationListener监听器，根据supportsEventType方法绑定事件，通过onApplicationEvent方法触发事件监听逻辑。

--- src/main/java/com/sb/sb2/Sb2Application.java
+++ src/main/java/com/sb/sb2/Sb2Application.java
@@ -2,6 +2,7 @@ package com.sb.sb2;
 
 import com.sb.sb2.initializer.SecondInitializer;
 import com.sb.sb2.listener.SecondListener;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +11,13 @@ import javax.swing.*;
 
 @SpringBootApplication
 @MapperScan(value = "com.sb.sb2.mapper")
+@Slf4j
 public class Sb2Application {
 
     public static void main(String[] args) {
          SpringApplication.run(Sb2Application.class, args);
+         log.info("Start Run Success...");
+
         //创建SB应用
 //        SpringApplication springApplication = new SpringApplication(Sb2Application.class);
 //        //添加系统化初始器
--- src/main/java/com/sb/sb2/listener/FourthListener.java
+++ src/main/java/com/sb/sb2/listener/FourthListener.java
@@ -1,12 +1,44 @@
 package com.sb.sb2.listener;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.SmartApplicationListener;
+import org.springframework.core.annotation.Order;
+
 /**
  * Created by ipipman on 2020/8/22.
  *
  * @version V1.0
  * @Package com.sb.sb2.listener
- * @Description: (用一句话描述该文件做什么)
+ * @Description: (SmartApplicaitonListener系统监听器，根据自己指定事件进行监听)
  * @date 2020/8/22 5:54 下午
  */
-public class FourthListener  {
+@Order(4)
+@Slf4j
+public class FourthListener implements SmartApplicationListener {
+
+    /**
+     * 授权对哪些时间感兴趣
+     *
+     * @param eventType
+     * @return
+     */
+    @Override
+    public boolean supportsEventType(Class<? extends ApplicationEvent> eventType) {
+        return ApplicationStartedEvent.class.isAssignableFrom(eventType)
+                || ApplicationPreparedEvent.class.isAssignableFrom(eventType);
+    }
+
+    /**
+     * 触发事件
+     *
+     * @param event
+     */
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        log.info("自定义【监听器】【监听SpringBoot启动事件】【FourthListener】启动 ...");
+    }
 }
+
